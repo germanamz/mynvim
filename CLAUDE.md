5f64100bdd124d8d7952c78e96c2b308dc2d30f5
German Meza <iam@germanamz.com>
2025-07-31 16:53:47 -0600

    buffer tabs

--- CLAUDE.md
+++ CLAUDE.md
@@ -3,4 +3,5 @@ This project is my neovim config
 
 ## Rules for changes
 * When a new keybinding is added/created it should be added to the auto-documentation.
+* When I work on any project I normally navigate with either nvim-tree or Telescope, so for any new config regarthing buffers or similar need to consider both plugins.
 
--- init.lua
+++ init.lua
@@ -737,6 +737,40 @@ vim.api.nvim_create_autocmd({"BufEnter", "DirChanged"}, {
   end,
 })
 
+-- Configure tabline to show all buffers as tabs
+vim.opt.showtabline = 2  -- Always show tabline
+
+-- Custom tabline function to show buffers as tabs
+function _G.custom_tabline()
+  local tabline = ''
+  local current_buf = vim.api.nvim_get_current_buf()
+  
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.api.nvim_buf_is_loaded(buf) and vim.api.nvim_buf_get_option(buf, 'buflisted') then
+      local bufname = vim.api.nvim_buf_get_name(buf)
+      local buftype = vim.api.nvim_buf_get_option(buf, 'buftype')
+      
+      -- Skip special buffers
+      if buftype == '' and bufname ~= '' and not bufname:match('NvimTree') then
+        local filename = vim.fn.fnamemodify(bufname, ':t')
+        if filename == '' then filename = '[No Name]' end
+        
+        -- Add modified indicator
+        local modified = vim.api.nvim_buf_get_option(buf, 'modified') and '*' or ''
+        
+        -- Highlight current buffer
+        local highlight = (buf == current_buf) and '%#TabLineSel#' or '%#TabLine#'
+        
+        tabline = tabline .. highlight .. ' ' .. buf .. ':' .. filename .. modified .. ' %#TabLine#|'
+      end
+    end
+  end
+  
+  return tabline .. '%#TabLineFill#'
+end
+
+vim.opt.tabline = '%!v:lua.custom_tabline()'
+
 -- Generate help tags for custom documentation
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = function()
